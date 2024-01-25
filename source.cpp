4301e38f5ad10460ddb52e51553757a67e871ead
mcookie47 <57654936+mcookie47@users.noreply.github.com>
2024-01-24 23:03:31 -0800

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -2,20 +2,19 @@
 using namespace std;
 void pointer()
 {
-int n[3] = { 5, 25, 125 };
+	int n[3] = { 5, 25, 125 };
 
-  int* ptr;
+	int* ptr;
 
-  ptr = n;
+	ptr = n;
 
-  for (int i = 0; i < 3; i++) {
-cout << "Value at ptr = << ptr << endl;
-cout << "Value at ptr* = << *ptr << endl;
+	for (int i = 0; i < 3; i++) {
+		cout << "Value at ptr = " << ptr << endl;
+		cout << "Value at ptr* = " << *ptr << endl;
 
-  // increment pointer by 1
-  ptr++
-  }
-  
+		// increment pointer by 1
+		ptr++;
+	}
 }
 
 //Driver program
