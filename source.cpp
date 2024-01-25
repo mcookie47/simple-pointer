124111f54d449bee58ea9c6cc86bb78cffab43a7
mcookie47 <57654936+mcookie47@users.noreply.github.com>
2024-01-24 23:23:57 -0800

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -15,7 +15,25 @@ void pointer()
 		// increment pointer by 1
 		ptr++;
 	}
+
+	cout << endl << endl;
+	//pointers are symbolic representations of addresses
+
+	int var2 = 7;
+
+	//declare pointer variable
+	int* ptr2;
+
+	//note that data type of ptr2 and var must be the same
+
+	ptr2 = &var2;
+
+	//assign the address of a variable to a pointer
+	cout << "Value at ptr2 = " << ptr << endl;
+	cout << "Value at var2 = " << var2 << endl;
+	cout << "Value at *ptr2 = " << *ptr2 << endl;
+
 }
 
 //Driver program
-int main() { pointer(); }
+int main() { pointer(); return 0;  }
